e58bec20e3a6b8b8908d1a1d11398401e14e5051
Tess Gompper <tgomp9@gmail.com>
2023-12-11 15:26:28 -0500

    try headers fix

--- app.js
+++ app.js
@@ -15,8 +15,13 @@ const settings = require("./Routes/api/settings");
 const app = express();
 connectDB();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://polite-quokka-e29580.netlify.app');
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://polite-quokka-e29580.netlify.app');
+//   next();
+// });
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
